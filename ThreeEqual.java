984dc07ef163df8df7f15c6de93b31f3ccf68007
Charles Sharp <25charlessharp@usd266.com>
2023-01-12 12:02:25 -0600

    Finishing touches

--- ThreeEqual.java
+++ ThreeEqual.java
@@ -1,5 +1,16 @@
 public class ThreeEqual {
     public static void main(String[] args) {
-        
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        if (args.length != 3) {
+            System.out.println("Please give arguments ");
+        }
+        if(a == b && b == c) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
     }
 }
