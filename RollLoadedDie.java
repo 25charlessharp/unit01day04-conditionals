a001efcebc1bbf798fa5e0655ba053a3f0f3da9c
Charles Sharp <25charlessharp@usd266.com>
2023-01-13 11:22:41 -0600

    testing it out

--- RollLoadedDie.java
+++ RollLoadedDie.java
@@ -2,6 +2,18 @@ public class RollLoadedDie {
     public static void main(String[] args) {
         Double test = Math.random();
         
-        System.out.println();
+        if (test <= 0.125){
+            System.out.println("You rolled a one");
+        } else if (test <= 0.250){
+            System.out.println("You rolled a two");
+        } else if (test <= 0.375){
+            System.out.println("You rolled a three");
+        } else if (test <= 0.5){
+            System.out.println("You rolled a four");
+        } else if (test <= 0.625){
+            System.out.println("You rolled a five");
+        } else{
+            System.out.println("You rolled a six");
+        }
     }
 }
