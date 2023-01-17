62329671c8f40c517cdbe73d11ae3aeccfb2a722
Charles Sharp <25charlessharp@usd266.com>
2023-01-17 11:35:13 -0600

    arrrays | accessing (FirstLast06)

---
+++ Day05Arrays.java
@@ -0,0 +1,11 @@
+public class Day05Arrays {
+    public static void main(String[] args) {
+        
+        int[] a = {4,6,1,2,3} ;
+        if (a [0] == 6 || a[a.length-1] == 6){
+            System.out.println(true);
+        }else {
+            System.out.println(false);
+        }
+    }
+}
